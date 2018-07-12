1fc7a3fff6fa69cb5aa8d9cb28b200f14ffa27af
José Cadena <cadena@tejuino.mx>
2018-07-11 21:09:35 -0500

    segunda clase

---
+++ TabBar.swift
@@ -0,0 +1,35 @@
+//
+//  TabBar.swift
+//  First Class
+//
+//  Created by José Cadena on 10/07/18.
+//  Copyright © 2018 José Cadena. All rights reserved.
+//
+
+import UIKit
+
+class TabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
